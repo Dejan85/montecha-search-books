0c65988508ce04e0a5271ed79403d5f742c3040b
Dejan <33637358+Dejan85@users.noreply.github.com>
2021-07-12 23:25:57 +0200

    created button UI component

--- src/frontend/search-page/SearchPage.tsx
+++ src/frontend/search-page/SearchPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import { Form, Input, Label } from "src/ui";
+import { Form, Input, Label, Button } from "src/ui";
 import { FormValue } from "./types";
 
 const SearchPage: React.FC = (): JSX.Element => {
@@ -11,12 +11,21 @@ const SearchPage: React.FC = (): JSX.Element => {
     formState: { errors },
   } = useForm<FormValue>();
 
-  const onSubmit = () => {};
+  interface searchTypes {
+    search: string;
+  }
+
+  const onSubmit = ({ search }: searchTypes): void => {
+    console.log("test", search);
+  };
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
       <Label htmlFor="email">Search</Label>
       <Input type="text" register={{ ...register("search") }} />
+      <Button width="100%" type="submit">
+        Search
+      </Button>
     </Form>
   );
 };
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,3 +1,4 @@
 export { Input } from "./input";
 export { Form } from "./form";
 export { Label } from "./label";
+export { Button } from "./button";
